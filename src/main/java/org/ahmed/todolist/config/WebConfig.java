a1e902596ed294ec044de1c82832952939260b30
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-05-26 15:58:05 +0200

    ajouter le serveur angular dans la liste des origines autorisées

---
+++ src/main/java/org/ahmed/todolist/config/WebConfig.java
@@ -0,0 +1,15 @@
+package org.ahmed.todolist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(new String[]{"http://localhost:4200"});
+    }
+}
