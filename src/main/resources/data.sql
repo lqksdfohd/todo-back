86630a18da861cf468a980b4e11067dcc0699443
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-05-26 16:17:40 +0200

    feature: detail a todo
    augmenter le jeu de données

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into todo(id, titre, description, faite) values(1, 'creer le projet todo', 'créer les scripts ddl et dml',1);
 insert into todo(id, titre, description, faite) values(2, 'tester le projet todo', 'faire les tests unitaires et d intégrations',0);
+insert into todo(id, titre, description, faite) values(3, 'todo n°3', 'faire la todo numéro trois',0);
+insert into todo(id, titre, description, faite) values(4, 'todo n°4', 'faire la todo numéro quatre',0);
+insert into todo(id, titre, description, faite) values(5, 'todo n°5', 'faire la todo numéro cinq',0);
+insert into todo(id, titre, description, faite) values(6, 'todo n°6', 'faire la todo numéro six',0);
+insert into todo(id, titre, description, faite) values(7, 'todo n°7', 'faire la todo numéro sept',0);
+insert into todo(id, titre, description, faite) values(8, 'todo n°8', 'faire la todo numéro huit',0);
